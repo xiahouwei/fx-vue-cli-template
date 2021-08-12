ed3494122b202662dcec91c8e8c402413b758316
商华伟 <xihaouwei@aliyun.com>
2021-08-12 22:02:02 +0800

    task: 增加ask文件

---
+++ ask.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'appName',
+        message: 'Project name'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description'
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'Project author'
+    }
+]
