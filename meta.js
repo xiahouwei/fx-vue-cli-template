77a076234c48b4fc2f21a3f6c66bc30fcc7b254b
商华伟 <xiahouwei@aliyun.com>
2022-02-27 11:22:57 +0800

    change

--- meta.js
+++ meta.js
@@ -14,6 +14,23 @@ module.exports = {
             type: 'input',
             name: 'author',
             message: 'Project author'
-        }
+        },
+        router: {
+            type: 'confirm',
+            message: 'use vue-router((路由功能模块)?',
+        },
+        i18n: {
+            type: 'confirm',
+            message: 'use vue-i18n(国际化功能模块)?',
+        },
+        fxService: {
+            type: 'confirm',
+            message: 'use fx-service(风行http请求功能模块)?',
+        },
+        fxLogger: {
+            type: 'confirm',
+            message: 'use fx-logger(风行日志功能模块)?',
+        },
+
     }
 }
